dded7f741004f99d8dd29cf545bb7fe1c7f61b62
Andy <andytruong94@gmail.com>
2018-01-05 19:14:59 +0100

    it works

--- mangastream/openlink.py
+++ mangastream/openlink.py
@@ -17,17 +17,43 @@ def parse_html(website):
     html = response.read()
     return BeautifulSoup(html, "lxml")
 
+def is_pref_manga(manga_list, text):
+    for manga in manga_list:
+        if manga in text:
+            return True
+    return False
+
 def main(args):
     if len(sys.argv) > 1:
         filter = (' ').join(sys.argv[1:])
     else:
         filter = 'today'
+
+    pref_manga = {
+        'Akatsuki no Yona',
+        'All Out',
+        'Black Clover',
+        'Boruto',
+        'Dr. Stone',
+        'Grashros',
+        'Haikyuu',
+        'My Hero Academia',
+        'One Piece',
+        'One-Punch Man',
+        'Robot x Laserbeam',
+        'Shokugeki no Souma',
+        'The Seven Deadly Sins',
+        'Tokyo Ghoul:re',
+        'Ubel Blatt',
+        'Vinland Saga'
+        }
     website = 'https://readms.net/'
+
     parsed_html = parse_html(website)
     latest_manga = parsed_html.body.find('ul', attrs={'class':'new-list'})
     for manga in latest_manga.find_all('li'):
         date = manga.find('span')
-        if filter in date.text.lower():
+        if filter in date.text.lower() and is_pref_manga(pref_manga, manga.find('a').text):
             webbrowser.get('firefox.exe %s').open(website + manga.find('a').get('href'))
 
 if __name__ == '__main__':
