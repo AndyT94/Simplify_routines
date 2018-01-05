31a87186816360741220356177f5b62ef1b33739
Andy <andytruong94@gmail.com>
2018-01-05 18:39:42 +0100

    refactor

--- mangastream/openlink.py
+++ mangastream/openlink.py
@@ -9,21 +9,26 @@ import urllib.request
 import webbrowser
 import sys
 
-if len(sys.argv) > 2:
-    filter = (' ').join(sys.argv[1:])
-else:
-    filter = 'today'
+def parse_html(website):
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    header = {'User-Agent':user_agent,}
+    request = urllib.request.Request(website, headers=header)
+    response = urllib.request.urlopen(request)
+    html = response.read()
+    return BeautifulSoup(html, "lxml")
 
-user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-header = {'User-Agent':user_agent,}
-website = 'https://readms.net/'
-request = urllib.request.Request(website, headers=header)
-response = urllib.request.urlopen(request)
-html = response.read()
-parsed_html = BeautifulSoup(html, "lxml")
+def main(args):
+    website = 'https://readms.net/'
+    parsed_html = parse_html(website)
+    latest_manga = parsed_html.body.find('ul', attrs={'class':'new-list'})
+    for manga in latest_manga.find_all('li'):
+        date = manga.find('span')
+        if filter in date.text.lower():
+            webbrowser.get('firefox.exe %s').open(website + manga.find('a').get('href'))
 
-latest_manga = parsed_html.body.find('ul', attrs={'class':'new-list'})
-for manga in latest_manga.find_all('li'):
-    date = manga.find('span')
-    if filter in date.text.lower():
-        webbrowser.get('firefox.exe %s').open(website + manga.find('a').get('href'))
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        filter = (' ').join(sys.argv[1:])
+    else:
+        filter = 'today'
+    main(filter)
